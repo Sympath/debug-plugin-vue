9381ea0bd0f58a81f6b0dea4c3e4b2ac60d6c3ed
Wzyan <3101885298@qq.com>
2021-07-30 03:14:05 +0800

    支持线上环境开启插件

--- main.js
+++ main.js
@@ -9,16 +9,24 @@ export default  function pluginWrapper(options) {
     isMapp,
     isDev
   } = options;
-  // 如果是dev环境 才继续执行
-  if(callFn(isDev,window.location)){
-    // 如果是子应用 则加载子应用的插件
-    if(isMapp){
+  window.vmDebugPlugin = {
+    open(){
+      initPlugin()
+    }
+  }
+  function initPlugin() {
+     // 如果是子应用 则加载子应用的插件
+     if(isMapp){
       return mappPluginFn(options)
     }
     // 否则 加载主应用的插件
     else{
       return vmPluginFn(options)
     }
+  }
+  // 如果是dev环境 才继续执行
+  if(callFn(isDev,window.location)){
+    initPlugin();
   }else {
     return {
       install(){
